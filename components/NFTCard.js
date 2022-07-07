24a985727cd89f26db3e15de617dd5fa6eb4bf34
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-07 16:10:07 +0530

    nft price info added

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -33,6 +33,26 @@ const NFTCard = ({ data }) => {
       </View>
 
       <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton />
+        </View>
+      </View>
     </View>
   );
 };
