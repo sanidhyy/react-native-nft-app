c4561698de7086ac9b4f10ffccbc084276461bb8
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-08 13:56:36 +0530

    details header added

--- screens/Details.js
+++ screens/Details.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   View,
   Text,
@@ -17,8 +18,75 @@ import {
   DetailsBid,
 } from "../components";
 
-const Details = () => {
-  return <Text>Details</Text>;
+const DetailsHeader = ({ data, navigation }) => (
+  <View
+    style={{
+      width: "100%",
+      height: 373,
+    }}
+  >
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+    <CircleButton
+      imgUrl={assets.heart}
+      handlePress={() => navigation.goBack()}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+  </View>
+);
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Details;
